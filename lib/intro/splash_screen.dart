9808d88765aec0173f41073c33145a09f4eedf35
kaushal <kaushalvadi7777@gmail.com>
2025-03-03 11:48:28 +0530

    Update Splash screen with lottie animation

--- lib/intro/splash_screen.dart
+++ lib/intro/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:twitter_clone/services/auth/auth_gate.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,30 +9,9 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _opacityAnimation;
-  late Animation<double> _scaleAnimation;
-
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-
-    _opacityAnimation = Tween<double>(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
-
-    _scaleAnimation = Tween<double>(
-      begin: 0.3,
-      end: 1.1,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutExpo));
-
-    _controller.forward();
 
     Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
@@ -42,37 +22,101 @@ class _SplashScreenState extends State<SplashScreen>
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Center(
-          child: AnimatedBuilder(
-            animation: _controller,
-            builder: (context, child) {
-              return Opacity(
-                opacity: _opacityAnimation.value,
-                child: Transform.scale(
-                  scale: _scaleAnimation.value,
-                  child: child,
-                ),
-              );
-            },
-            child: Image.asset(
-              'assets/images/twitter_logo.jpg', // Ensure the logo is in assets
-              width: 150,
-              height: 150,
-              filterQuality: FilterQuality.high,
-            ),
-          ),
+      backgroundColor: Colors.black, // Set full black background
+      body: Center(
+        child: Lottie.asset(
+          'assets/animations/twitter_logo_animation.json', // Path to Lottie animation
+          width: 250,
+          height: 350,
+          fit: BoxFit.contain,
+          repeat: false, // Plays once
         ),
       ),
     );
   }
 }
+
+
+//Splash screen with logo animation
+
+// import 'package:flutter/material.dart';
+// import 'package:twitter_clone/services/auth/auth_gate.dart';
+
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen>
+//     with SingleTickerProviderStateMixin {
+//   late AnimationController _controller;
+//   late Animation<double> _opacityAnimation;
+//   late Animation<double> _scaleAnimation;
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     _controller = AnimationController(
+//       duration: const Duration(seconds: 2),
+//       vsync: this,
+//     );
+
+//     _opacityAnimation = Tween<double>(
+//       begin: 0.0,
+//       end: 1.0,
+//     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+//     _scaleAnimation = Tween<double>(
+//       begin: 0.3,
+//       end: 1.1,
+//     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutExpo));
+
+//     _controller.forward();
+
+//     Future.delayed(const Duration(seconds: 3), () {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => AuthGate()),
+//       );
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         color: Colors.black,
+//         child: Center(
+//           child: AnimatedBuilder(
+//             animation: _controller,
+//             builder: (context, child) {
+//               return Opacity(
+//                 opacity: _opacityAnimation.value,
+//                 child: Transform.scale(
+//                   scale: _scaleAnimation.value,
+//                   child: child,
+//                 ),
+//               );
+//             },
+//             child: Image.asset(
+//               'assets/images/twitter_logo.jpg', // Ensure the logo is in assets
+//               width: 150,
+//               height: 150,
+//               filterQuality: FilterQuality.high,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
